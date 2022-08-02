ea55e82cda04ae45708c3d9acc1f657a4ff86b34
Jose Garzon <jd.garzon12@gmail.com>
2022-08-02 17:08:05 -0500

    ✨ add the google sign in handler

--- components/Atoms/Button/GoogleButton.tsx
+++ components/Atoms/Button/GoogleButton.tsx
@@ -1,9 +1,17 @@
 import { Button, ButtonProps } from "@nextui-org/react";
 import { TbBrandGoogle } from "react-icons/tb";
+import { signIn } from "next-auth/react";
 
 const GoogleButton = (props: Partial<ButtonProps>) => {
+  const googleSignIn = () => signIn("google");
   return (
-    <Button rounded color="error" icon={<TbBrandGoogle size="20" />} {...props}>
+    <Button
+      rounded
+      color="error"
+      icon={<TbBrandGoogle size="20" />}
+      onClick={googleSignIn}
+      {...props}
+    >
       Entrar con Google
     </Button>
   );
